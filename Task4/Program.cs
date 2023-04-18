b23e05b38d3f8b2c4ed9ce344c3d09c3852b43f6
prollla <skull.boy@rambler.ru>
2023-04-18 21:52:17 +0300

    4 and 5

---
+++ Task4/Program.cs
@@ -0,0 +1,64 @@
+﻿class ObjectQualityCalculator
+{
+    private double x;
+    private double y;
+    private double quality;
+
+    public ObjectQualityCalculator(double xValue, double yValue)
+    {
+        x = xValue;
+        y = yValue;
+        CalculateQuality();
+    }
+
+    private void CalculateQuality()
+    {
+        quality = 0.1 * x * y;
+    }
+
+    public void PrintObjectInfo()
+    {
+        Console.WriteLine("Object quality information:");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("y = " + y);
+        Console.WriteLine("Quality = " + quality);
+    }
+    
+    class UpdatedObjectQualityCalculator : ObjectQualityCalculator
+    {
+        private double z;
+        private double v;
+
+        public UpdatedObjectQualityCalculator(double xValue, double yValue, double zValue, double vValue) : base(xValue, yValue)
+        {
+            z = zValue;
+            v = vValue;
+            RecalculateQuality();
+        }
+
+        private void RecalculateQuality()
+        {
+            double updatedQuality = quality - 1.5 * (z - v);
+            quality = updatedQuality > 0 ? updatedQuality : 0;
+        }
+        public void PrintUpdatedObjectInfo()
+        {
+            Console.WriteLine("Updated object quality information:");
+            //Console.WriteLine("x = " + x);
+            //Console.WriteLine("y = " + y);
+            Console.WriteLine("z = " + z);
+            Console.WriteLine("v = " + v);
+            Console.WriteLine("Quality = " + quality);
+        }
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        ObjectQualityCalculator obj = new ObjectQualityCalculator(5.0, 10.0);
+        obj.PrintObjectInfo();
+        UpdatedObjectQualityCalculator obj1 = new UpdatedObjectQualityCalculator(5.0, 10.0, 2000.0, 1999.0);
+        obj1.PrintUpdatedObjectInfo();
+
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+public interface IPaper
+{
+    string Text { get; set; }
+
+    void Write(string text);
+    void Clear();
+}
+public abstract class AbstractPaper : IPaper
+{
+    public string Text { get; set; }
+
+    protected int NumberOfPages { get; set; }
+
+    protected string PaperType { get; set; }
+
+    public abstract void Write(string text);
+
+    public virtual void Clear()
+    {
+        Text = "";
+    }
+    public abstract void AddPage();
+}
+public class RegularPaper : AbstractPaper
+{
+    public string Author { get; set; }
+
+    public int Year { get; set; }
+
+    public override void Write(string text)
+    {
+        Text += text;
+    }
+
+    public override void AddPage()
+    {
+        NumberOfPages++;
+    }
+
+    public void SetType(string paperType)
+    {
+        PaperType = paperType;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Text: {0}", Text);
+        Console.WriteLine("Number of Pages: {0}", NumberOfPages);
+        Console.WriteLine("Type: {0}", PaperType);
+        Console.WriteLine("Author: {0}", Author);
+        Console.WriteLine("Year: {0}", Year);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        List<IPaper> papers = new List<IPaper>();
+
+        // Создаем экземпляры класса RegularPaper и добавляем их в список
+        RegularPaper paper1 = new RegularPaper();
+        paper1.SetType("Lined paper");
+        paper1.Author = "John Smith";
+        paper1.Year = 2021;
+        paper1.Write("Hello, world!");
+        papers.Add(paper1);
+
+        RegularPaper paper2 = new RegularPaper();
+        paper2.SetType("Blank paper");
+        paper2.Author = "Jane Doe";
+        paper2.Year = 2022;
+        paper2.Write("Lorem ipsum dolor sit amet");
+        paper2.AddPage();
+        paper2.Write(", consectetur adipiscing elit");
+        papers.Add(paper2);
+        
+        foreach (IPaper paper in papers)
+        {
+            if (paper is RegularPaper)
+            {
+                RegularPaper regularPaper = (RegularPaper)paper;
+                regularPaper.Print();
+            }
+        }
+    }
+}
